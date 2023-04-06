cc01d1a78564270952cd2acd869ad403bc396805
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-04-06 21:54:20 +0530

    Connecting our own api to get,post,delete the data

--- index.js
+++ index.js
@@ -1,70 +1,89 @@
-let expenseAmount=document.querySelector('#expenseAmount');
-let desc=document.querySelector('#desc');
-let category=document.querySelector('#categoryExpense');
-let itemList=document.querySelector('#listItem');
-let form=document.querySelector('form');
-let items=document.querySelectorAll('.list-group-item');
-form.addEventListener('submit',formExpense);
+let expenseAmount = document.querySelector('#expenseAmount');
+let desc = document.querySelector('#desc');
+let category = document.querySelector('#categoryExpense');
+let itemList = document.querySelector('#listItem');
+let form = document.querySelector('form');
+let items = document.querySelectorAll('.list-group-item');
+form.addEventListener('submit', formExpense);
+
+//get data
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get('http://localhost:3000/expense/get-expense-data')
+        .then((res) => {
+            for (var i = 0; i < res.data.length; i++) {
+                showOutput(res.data[i]);
+            }
+        })
+        .catch(err => console.log(err));
+})
 
 //submit expense function
-function formExpense(e){
+function formExpense(e) {
     e.preventDefault();
+    //save in the local storage
+    let object = {
+        amount: expenseAmount.value,
+        description: desc.value,
+        category: category.value
+    }
+    axios.post('http://localhost:3000/expense/add-expense-data', object)
+        .then((res) => {
+            showOutput(res.data);
+        })
+        .catch(err => console.log(err));
+    //clear the value in the form
+    expenseAmount.value = '';
+    desc.value = '';
+}
+
+const delFun = (id) => {
+    axios.delete(`http://localhost:3000/expense/delete-expense-data/${id}`)
+        .then((res) => {
+            removeFromScreen(res.data.id);
+        })
+        .catch(err => console.log(err));
+}
+
+function editFun(data) {
+    axios.delete(`http://localhost:3000/expense/delete-expense-data/${data.id}`)
+        .then((res) => {
+            removeFromScreen(res.data.id);
+        })
+        .catch(err => console.log(err));
+    expenseAmount.value = data.amount;
+    desc.value = data.description;
+}
+
+function showOutput(data) {
     //creating list
-    let li=document.createElement('li');
-    let text1=`${expenseAmount.value} - ${desc.value} - ${category.value} `;
-    li.className="list-group-item";
+    let li = document.createElement('li');
+    li.id = data.id;
+    let text1 = `${data.amount} - ${data.description} - ${data.category} `;
+    li.className = "list-group-item";
     li.append(text1);
-    let delBtn=document.createElement('button');
-    let text2=`Delete Expense`;
-    delBtn.className="btn btn-sm btn-outline-danger";
+    let delBtn = document.createElement('button');
+    delBtn.onclick = function () {
+        delFun(data.id);
+    }
+    let text2 = `Delete Expense`;
+    delBtn.className = "btn btn-sm btn-outline-danger";
     delBtn.append(text2);
-    let editBtn=document.createElement('button');
-    let text3=`Edit Expense`;
-    editBtn.className="btn btn-sm btn-outline-primary";
+    let editBtn = document.createElement('button');
+    editBtn.onclick = function () {
+        editFun(data);
+    }
+    let text3 = `Edit Expense`;
+    editBtn.className = "btn btn-sm btn-outline-primary";
     editBtn.append(text3);
     li.append(delBtn);
     li.append(editBtn);
     itemList.append(li);
-    //save in the local storage
-    let obj={
-        key:expenseAmount.value,
-        desc:desc.value,
-        category:category.value,
-    }
-    localStorage.setItem(obj.key,JSON.stringify(obj));
-    //clear the value in the form
-    expenseAmount.value='';
-    desc.value='';
 }
-itemList.addEventListener('click',btnFun);
-// button function
-function btnFun(e){
-    if(e.target.classList.contains('btn-outline-danger')){
-        let str=e.target.parentElement.firstChild.textContent;
-        let arr1=str.split('-');
-        arr1=arr1.map(i=>Number(i));
-        delStor(arr1[0]);
-        e.target.parentElement.remove();
+
+function removeFromScreen(id) {
+    let child = document.getElementById(id);
+    if (child) {
+        itemList.removeChild(child);
     }
-    else if(e.target.classList.contains('btn-outline-primary')){
-        let str=e.target.parentElement.firstChild.textContent;
-        let arr1=str.split('-');
-        let arr2=str.split('-');
-        arr1=arr1.map(i=>Number(i));
-        delStor(arr1[0]);
-        expenseAmount.value=arr1[0];
-        desc.value=arr2[1];
-        e.target.parentElement.remove();
-    }
+
 }
-// delete from localstorage
-function delStor(value){
-    let a=Object.keys(localStorage);
-    a=a.map(i=>Number(i));
-    a.forEach((val)=>{
-        if(val==value){
-            localStorage.removeItem(JSON.stringify(val));
-        }
-    })
-}
-localStorage.clear();
